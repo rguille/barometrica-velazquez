d3a797a67964b33257248cefd12d7a0fc575ac18
rguille <rguille@gmail.com>
2026-07-03 19:22:09 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,29 +16,29 @@ const CACHE_NAME = "velazquez-puntos-v2"
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
-event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-);
-self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-event.waitUntil(
-caches.keys().then((keys) =>
-Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-)
-);
-self.clients.claim();
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-event.respondWith(
-fetch(event.request)
-.then((response) => {
-const copy = response.clone();
-caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-return response;
-})
-.catch(() => caches.match(event.request))
-);
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
